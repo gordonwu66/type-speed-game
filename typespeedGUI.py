c075e52e1192df4afca3ead22c90f4cee1f02b14
gordonwu66 <gordonwu66@gmail.com>
2019-01-16 07:22:43 -0500

    gui

---
+++ typespeedGUI.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+def generateWord():
+	# UNIMPLEMENTED
+
+# Summons words, includes entry space, and facilitates game logic and loop
+def main():
+	# UNIMPLEMENTED
+	window = Tk()
+
+main()
+
+
+
