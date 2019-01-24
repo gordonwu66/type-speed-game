7cb1f3b7c781130eb314d9de57e20804a76c9cff
gordonwu66 <gordonwu66@gmail.com>
2019-01-24 07:33:53 -0500

    gui

--- typespeedGUI.py
+++ typespeedGUI.py
@@ -1,12 +1,18 @@
+# Possible GUI implementation for the typespeed.py game
+
 from tkinter import *
 
 def generateWord():
 	# UNIMPLEMENTED
+	print('word')
 
 # Summons words, includes entry space, and facilitates game logic and loop
 def main():
 	# UNIMPLEMENTED
 	window = Tk()
+	window.title('Typespeed Game')
+	frame = Frame(window)
+	window.mainloop()
 
 main()
 
